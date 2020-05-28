a596087cb65a9eedfaea49aa011eb26ba91f2fb5
Ono <soichrooono@gmail.com>
2020-05-28 05:30:54 +0000

    [Add]refile

--- db/migrate/20200528025245_devise_create_users.rb
+++ db/migrate/20200528025245_devise_create_users.rb
@@ -32,10 +32,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
-      t.timestamps null: false
       t.string :name
       t.text :introduction
+      t.timestamps null: false
+
     end
 
     add_index :users, :email,                unique: true
