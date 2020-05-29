500639d08b496e6dc7aaa5eb456e9bcad8d8ef96
Ono <soichrooono@gmail.com>
2020-05-29 10:13:51 +0000

    [Update]User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,39 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user!, only:[:show, :edit, :update]
+	before_action :ensure_correct_user, only:[:edit, :update]
+	def ensure_correct_user
+		@user = User.find(params[:id])
+		if current_user != @user
+			reidrect_to posts_path
+		end
+	end
+
+
 	def top
 	end
 
 	def show
+		@user = User.find(params[:id])
+		@poss = @user.posts.order(id:"DESC")
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:notice] = "UPDATE YOUR PROFILE !"
+			reidrect_to user_path(@user)
+		else
+			render("users/edit")
+		end
+	end
+
+	protected
+	def user_params
+		params.require(:user).permit(:name, :introduction, :profile_image)
 	end
 
 end
