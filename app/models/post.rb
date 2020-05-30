79f27e3808421548f8ada41ded755130fac86e0e
Ono <soichrooono@gmail.com>
2020-05-30 02:53:25 +0000

    [Add]Favorites

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,8 @@ class Post < ApplicationRecord
  	validates :title, presence:true ,length:{maximum: 20}
  	validates :body, presence:true ,length:{maximum: 200}
  	validates :post_image, presence:true
+
+ 	def favorited_by?(user)
+ 		favorites.where(user_id: user.id).exists?
+ 	end
 end
