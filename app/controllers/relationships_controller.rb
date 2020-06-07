31a5f52d0c52de36f2602fbb3088fb8f8ab884a9
Ono <soichrooono@gmail.com>
2020-06-07 10:16:44 +0000

    [Add]relation ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,21 +1,21 @@
 class RelationshipsController < ApplicationController
   def create
     current_user.follow(params[:user_id])
-    redirect_back(fallback_location: root_path)
+    @user = User.find(params[:user_id])
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_back(fallback_location: root_path)
+    @user = User.find(params[:user_id])
   end
 
   def follows
-    user = User.find(params[:user_id])
-    @users = user.following_user
+    @user = User.find(params[:user_id])
+    @users = @user.following_user
   end
 
   def followers
-    user = User.find(params[:user_id])
-    @users = user.follower_user
+    @user = User.find(params[:user_id])
+    @users = @user.follower_user
   end
 end
